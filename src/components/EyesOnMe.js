7b808f69806372d979a9aa657485214e30e3b595
Victor Kemboi <vickemtoo97@gmail.com>
2024-04-26 15:02:58 +0300

    event handling in react

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+const EyesOnMe=() =>{
+  const focusHandler=() =>{
+    console.log("Good!");
+  }
+
+ const blurHandler=()=> {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onFocus={focusHandler} onBlur={blurHandler}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
